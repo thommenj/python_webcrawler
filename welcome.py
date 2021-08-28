567da5d2c848b376399858f99e61599044d67921
Joël Thommen <thommen.joel@gmail.com>
2021-08-28 16:26:08 +0200

    initial commit, added welcome.py

---
+++ welcome.py
@@ -0,0 +1,7 @@
+# simple script which asks for the caller's name
+print('hey, funny to see you around these lands. what is your name fellow traveler?')
+
+name = input()
+
+print('well well well...', name, 'it is then')
+print('i hope you enjoy your stay')
